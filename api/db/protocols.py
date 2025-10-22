6c1084b890d739a20e695df52272ec7aabcd6cbd
Vadim Sokoltsov <vforvad@gmail.com>
2025-10-22 16:03:01 +0200

    Update storage protocol

--- api/db/protocols.py
+++ api/db/protocols.py
@@ -1,9 +1,9 @@
-from typing import Protocol, runtime_checkable
+from typing import Protocol, runtime_checkable, List
 import pandas as pd
 
 @runtime_checkable
 class StorageBackend(Protocol):
     """Protocol defining the interface for storage backends"""
 
-    def prepare_for_verification(self, trx_id: int) -> pd.DataFrame:
+    def prepare_for_verification(self, trx_id: int, features: List[str]) -> pd.DataFrame:
         ...
