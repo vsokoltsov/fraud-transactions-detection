09d70b74aa680fca79de151298754f64889698e1
Vadim Sokoltsov <vforvad@gmail.com>
2025-10-22 12:28:15 +0200

    Update config

--- api/config.py
+++ api/config.py
@@ -1,9 +1,15 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import ClassVar
 
 class Settings(BaseSettings):
     storage: str
+    model: str
+    version: str
     storage_path: str
+    features_path: str
+    models_path: str
+    thresholds_path: str
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(env_file=".env")
 
-settings = Settings()
+settings = Settings() # type: ignore
